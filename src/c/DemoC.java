1e85e7d7c68be134edc302589dda626f4bc11666
samchapman <samchapman@192.168.1.245>
2024-01-07 23:59:10 -0500

    I love this

---
+++ src/c/DemoC.java
@@ -0,0 +1,14 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello again C");
+		
+		System.out.println("Testing 123...");
+		
+		System.out.println("testing abc...");
+	}
+
+}
